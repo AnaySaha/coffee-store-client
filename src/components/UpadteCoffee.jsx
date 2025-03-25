35d7687bf87090387b03b4e0c364be231d9dcadc
Anay Saha <anaysahapaban@gmail.com>
2025-03-26 03:12:14 +0600

    c

---
+++ src/components/UpadteCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpadteCoffee = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UpadteCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
+import './index.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffee from './components/AddCoffee.jsx';
+import UpadteCoffee from './components/UpadteCoffee.jsx';
 
 const router = createBrowserRouter([
   {
-  path: "/",
-  element: <div> Hellow World</div>,
-  }
-  ]);
+    path: "/",
+    element: <App></App>,
+  },
+{
+  path: "addCoffee",
+  element: <AddCoffee></AddCoffee>
+},
 
+{
+  path: 'updateCoffee',
+  element: <UpadteCoffee></UpadteCoffee>
+}
+
+]);
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

b3ec92e53f08274972b73c09c402b5c8279b2f44
Anay Saha <anaysahapaban@gmail.com>
2025-03-25 23:54:38 +0600

    push

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <div> Hellow World</div>,
+  }
+  ]);
+
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
